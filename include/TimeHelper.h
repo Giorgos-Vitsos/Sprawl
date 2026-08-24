248a6c62d54f38d7d5921ca7d1a501a373c31c2d
Giorgos Vitsos <csd5369@csd.uoc.gr>
2026-08-25 01:59:39 +0300

    Refactor InputController and TimeHelper to use static member naming convention

--- include/TimeHelper.h
+++ include/TimeHelper.h
@@ -3,9 +3,9 @@
 
 class TimeHelper{
     private:
-        static double m_CurrentFrame;
-        static double m_LastFrame;
-        static double m_DeltaTime;
+        static double s_CurrentFrame;
+        static double s_LastFrame;
+        static double s_DeltaTime;
     public:
         static void Tick();
         static double GetDeltaTime();
--- src/Core/TimeHelper.cpp
+++ src/Core/TimeHelper.cpp
@@ -1,14 +1,14 @@
 #include "TimeHelper.h"
 #include <GLFW/glfw3.h>
 
-double TimeHelper::m_CurrentFrame=0;
-double TimeHelper::m_LastFrame=0;
-double TimeHelper::m_DeltaTime=0;
+double TimeHelper::s_CurrentFrame=0;
+double TimeHelper::s_LastFrame=0;
+double TimeHelper::s_DeltaTime=0;
 
 void TimeHelper::Tick(){
-    m_CurrentFrame=glfwGetTime();
-    m_DeltaTime=m_CurrentFrame-m_LastFrame;
-    m_LastFrame=m_CurrentFrame;
+    s_CurrentFrame=glfwGetTime();
+    s_DeltaTime=s_CurrentFrame-s_LastFrame;
+    s_LastFrame=s_CurrentFrame;
 }
 
-double TimeHelper::GetDeltaTime(){ return m_DeltaTime;}
+double TimeHelper::GetDeltaTime(){ return s_DeltaTime;}
