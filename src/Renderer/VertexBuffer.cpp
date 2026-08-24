1e0681b9e18cc1bd5539dde7e87b96c902e6f960
Giorgos Vitsos <csd5369@csd.uoc.gr>
2026-08-25 02:40:16 +0300

    Add constructor overload and SetData method to VertexBuffer class

--- src/Renderer/VertexBuffer.cpp
+++ src/Renderer/VertexBuffer.cpp
@@ -1,10 +1,17 @@
 #include "VertexBuffer.h"
 #include <glad/glad.h>
+#include <stdexcept>
 
 VertexBuffer::VertexBuffer(const void*data, unsigned int size):m_Count(size){
     glGenBuffers(1,&m_ID);
     Bind();
     glBufferData(GL_ARRAY_BUFFER,size,data,GL_STATIC_DRAW);
+}
+
+VertexBuffer::VertexBuffer(unsigned int maxSize):m_Count(maxSize){
+    glGenBuffers(1,&m_ID);
+    Bind();
+    glBufferData(GL_ARRAY_BUFFER,maxSize,nullptr,GL_DYNAMIC_DRAW);
 }
 
 VertexBuffer::~VertexBuffer(){
@@ -32,3 +39,12 @@ void VertexBuffer::Bind() const{
 void VertexBuffer::Unbind() const{
     glBindBuffer(GL_ARRAY_BUFFER,0);
 }
+
+void VertexBuffer::SetData(const void *data,unsigned int size){
+    Bind();
+    if(size<=m_Count){
+        glBufferSubData(GL_ARRAY_BUFFER,0,size,data);
+    }else{
+        throw std::runtime_error("Data size exceeds the buffer\n");
+    }
+};
